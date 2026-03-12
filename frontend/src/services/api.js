c75c046176a9df9cbc542f84fdae358ab4ae8fb0
deepzsenu <deepzsenu@gmail.com>
2026-03-12 11:21:53 +0530

    updated bse url settings

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_BASEURL
 });
 
 API.interceptors.request.use((config) => {
@@ -13,6 +13,7 @@ API.interceptors.request.use((config) => {
   }
 
   return config;
+
 });
 
 export default API;
